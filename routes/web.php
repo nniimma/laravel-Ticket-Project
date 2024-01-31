214ab5a3200504744eded96d88381670e37c3336
Nima <alireza.khazforoosh@adventistas.org>
2024-01-31 11:49:47 -0300

    routing with shorter code

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-
-
 // ? the same code Route::get('/', function () {return view('welcome');});:
 Route::view('/', 'welcome');
